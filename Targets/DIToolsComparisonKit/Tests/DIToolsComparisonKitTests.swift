7066f1ff9ad9ded3705e7dc895a3c8ca7938ee32
taeminyun <taemin.yun@wesang.com>
2025-04-19 11:59:38 +0900

    init tuist

---
+++ Targets/DIToolsComparisonKit/Tests/DIToolsComparisonKitTests.swift
@@ -0,0 +1,8 @@
+import Foundation
+import XCTest
+
+final class DIToolsComparisonKitTests: XCTestCase {
+    func test_example() {
+        XCTAssertEqual("DIToolsComparisonKit", "DIToolsComparisonKit")
+    }
+}
